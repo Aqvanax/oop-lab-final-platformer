7a0bdbca2e45c37247d1c62a058132b7bb07cce1
Aqvanax <123380877+Aqvanax@users.noreply.github.com>
2026-05-17 02:21:16 +0700

    Use EDT for game initialization in MainClass
    Updated main method to use Event Dispatch Thread for game initialization.

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,8 +1,13 @@
 package main;
 
+import java.awt.EventQueue;
+
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("[Game] Booting Native Java...");
-        new Game();
+        System.out.println("[Game] Booting Native Java on EDT...");
+        
+        EventQueue.invokeLater(() -> {
+            new Game();
+        });
     }
 }
