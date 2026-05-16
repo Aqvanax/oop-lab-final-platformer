5a66c52c3084b38e30c6fcdd141e15fb8560d2a0
Aqvanax <123380877+Aqvanax@users.noreply.github.com>
2026-05-17 02:00:54 +0700

    Switch from AWT Frame to Swing JFrame
    Replaced AWT Frame with Swing JFrame for better compatibility and updated close operation.

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,30 +1,17 @@
 package main;
 
-import java.awt.Frame;
+import javax.swing.JFrame; // Dùng JFrame
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class GameWindow {
-
     public GameWindow(GamePanel gamePanel) {
-        Frame frame = new Frame();
-        
-        // frame.setUndecorated(true); // gây xung đột hiển thị trên Web
-        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(gamePanel);
         frame.pack();
-        
-        // frame.setLocationRelativeTo(null); // gây NullPointerException
-        
         frame.setResizable(false);
         frame.setVisible(true);
         gamePanel.requestFocus();
-
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
     }
 }
