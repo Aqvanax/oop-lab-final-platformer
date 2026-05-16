2f539914ddbaa183aa91774243cb559f57132315
Aqvanax <123380877+Aqvanax@users.noreply.github.com>
2026-05-17 02:11:21 +0700

    Switch from JFrame to AWT Frame in GameWindow
    Replaced JFrame with AWT Frame for the game window.

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,17 +1,26 @@
 package main;
 
-import javax.swing.JFrame; // Dùng JFrame
+import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class GameWindow {
+
     public GameWindow(GamePanel gamePanel) {
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Frame frame = new Frame();
+        
         frame.add(gamePanel);
         frame.pack();
+        
         frame.setResizable(false);
         frame.setVisible(true);
         gamePanel.requestFocus();
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
