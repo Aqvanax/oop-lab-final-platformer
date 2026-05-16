2b7c5241f6ad499a1c8fa5de0d8c3502dd0d26da
Aqvanax <123380877+Aqvanax@users.noreply.github.com>
2026-05-17 02:33:03 +0700

    Change boot message from Native Java to Swing

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -4,7 +4,7 @@ import java.awt.EventQueue;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("[Game] Booting Native Java on EDT...");
+        System.out.println("[Game] Booting Swing on EDT...");
         
         EventQueue.invokeLater(() -> {
             new Game();
